570506c77781ebc69b4fd6234ec998bfd6b53a0b
NatySRocha <natyrsr@gmail.com>
2021-08-25 12:31:09 -0300

    Exercicios

---
+++ demo/demo/src/main/java/demo/Exercicio1.java
@@ -0,0 +1,37 @@
+package demo;
+
+public class Exercicio1 {
+	HELP.md
+	alvo /
+	! .mvn / wrapper / maven-wrapper.jar
+	! ** / src / main / ** / target /
+	! ** / src / test / ** / target /
+
+	# ## STS ###
+	.apt_generated
+	.classpath
+	.factorypath
+	.projeto
+	.definições
+	.springBeans
+	.sts4-cache
+
+	# ## IntelliJ IDEA ###
+	.ideia
+	* .iws
+	* .iml
+	* .ipr
+
+	# ## NetBeans ###
+	/ nbproject / private /
+	/ nbbuild /
+	/ dist /
+	/ nbdist /
+	/ .nb-gradle /
+	construir /
+	! ** / src / main / ** / build /
+	! ** / src / test / ** / build /
+
+	# ## Código VS ###
+	.vscode /
+}
