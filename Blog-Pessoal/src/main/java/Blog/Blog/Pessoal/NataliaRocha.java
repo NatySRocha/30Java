20ce922f8f3702f11d866293968fd37f804f378e
NatySRocha <natyrsr@gmail.com>
2021-09-07 23:59:37 -0300

    Exercicios

---
+++ Blog-Pessoal/src/main/java/Blog/Blog/Pessoal/NataliaRocha.java
@@ -0,0 +1,50 @@
+package Blog.Blog.Pessoal;
+
+public class NataliaRocha {
+	Olá 👋 
+	### Olá! eu sou a Natalia Rocha 👋 
+
+	<! -
+	** NataliaRocha / NataliaRocha ** é um repositório ✨ _especial_ ✨ porque seu `README.md` (este arquivo) aparece em seu perfil GitHub.
+	- Atualmente estudando Java e MySQL.
+	- 🌱 Bootcamp Generation Brasil - Desenvolvedor FullStack
+	- 🎓 Tecnologo em Processos Gerenciais
+	- 📫 me contacte-me no email: natyrsr25@gmail.com
+	- 😄 Pronomes: Ela / dela
+	- 📍 Poa- SP
+
+	Aqui estão algumas idéias para você começar:
+
+	- 🔭 Atualmente estou trabalhando em ...
+	- 🌱 Atualmente estou aprendendo ...
+	- 👯 Estou procurando colaborar em ...
+	- 🤔 Estou procurando ajuda com ...
+	- 💬 Pergunte-me sobre ...
+	- 📫 Como chegar até mim: ...
+	- 😄 Pronomes: ...
+	- ⚡ Curiosidade: ...
+	->
+	<div>
+	  <a href="https://github.com/NataliaRocha">
+	  <img height = "180em" src = "https://github-readme-stats.vercel.app/api?username=NataliaRocha&show_icons=true&theme=radical&include_all_commits=true&count_private=true" />
+	  <img height = "180em" src = "https://github-readme-stats.vercel.app/api/top-langs/?username=NataliaRocha&layout=compact&langs_count=7&theme=radical" />
+	</div>
+
+	## 
+
+	<img align = "center" alt = "Naty-Java" height = "30" width = "40" src = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original -wordmark.svg ">
+	<img align = "center" alt = "Naty-MySQL" height = "30" width = "40" src = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original -wordmark.svg ">
+	<img align = "center" alt = "Naty-Spring" height = "30" width = "40" src = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original -wordmark.svg ">
+
+	##
+	<div>
+	   <a href = "mailto:natyrsr25@gmail.com"> <img src = https: //img.shields.io/badge/Gmail-D14836? style = for-the-badge & logo = gmail & logoColor = white
+	   <a href="https://http://www.linkedin.com/in/natalia-rocha-bb65abbb/ "target="_blank"> <img src = https: //img.shields.io/badge/LinkedIn-0077B5? style = for-the-badge & logo = linkedin & logoColor = white                                                           
+
+	  </div>
+
+
+
+
+
+}
